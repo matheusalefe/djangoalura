e1e39d47ae574422d88d382e626c6d47d48428ac
Matheus Alefe Munari Zanardi <matheus.orvati@hotmail.com>
2022-01-19 10:00:47 -0300

    testar recurso: itens dinamicos

--- receitas/views.py
+++ receitas/views.py
@@ -3,7 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+    
+
     return render(request,'index.html')
 
 def receitas(request):
-    return render(request, 'receitas.html')
+    receitas = {
+        1: 'Bolinho de chuva',
+        2: 'Omelete',
+        3: 'Carne moída',
+        4: 'Frango a passarinho'
+    }
+    dados = {
+        'nome_das_receitas': receitas
+    }
+
+    return render(request, 'receitas.html',dados)
